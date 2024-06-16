de31703bfe8cf9efc3099ffa7e7a7a02500c4575
murilog156 <murilog156@gmail.com>
2024-06-15 21:23:56 -0300

    b1506 2123

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// EXECUTA ESSA FUNÇÃO QUANDO EU CLICO EM QUALQUER BOTÃO DE TIPOS
+// E PASSA COMO PARAMETRO QUAL O BOTÃO QUE EU CLIQUEI
+// PARA EU PODER DECIDIR O QUE FAÇO CONFORME O TIPO QUE RECEBI
+function esconderProcSelecType(tipo) {
+
+    //ESCONDE A CAIXA DE POP UP
+    document.getElementById("inputboxprocselectype").style.display = "none"
+    // MOSTRA A CAIXA DOS INPUTS DO FORMULARIO
+    document.getElementById("caixa-dos-inputs-grande").style.display = "flex"
+
+
+    // SE O BOTÃO QUE FOI CLICADO FOI O BOTÃO DE PESSOA FISICA
+    if (tipo == "fisica") {
+        // EXECUTA ISSO ABAIXO
+        document.getElementById("caixa-dos-inputs-juridicos").style.display = "none"
+        document.getElementById("caixa-dos-inputs-fisicos").style.display = "flex"
+
+
+    }
+
+    // SE O BOTÃO QUE FOI CLICADO FOI O BOTÃO DE PESSOA JURIDICA
+    if (tipo == "juridica") {
+        // EXECUTA ISSO ABAIXO
+
+        document.getElementById("caixa-dos-inputs-fisicos").style.display = "none"
+        document.getElementById("caixa-dos-inputs-juridicos").style.display = "flex"
+    }
+}
+function retornaProcSelecType() {
+
+    document.getElementById("caixa-dos-inputs-grande").style.display = "none"
+    document.getElementById("inputboxprocselectype").style.display = "flex"
+
+}
+
+function openProcSelecType() {
+
+    document.getElementById("inputboxprocselectype").style.display = "flex"
+
+}
+
+function openRecibo() {
+
+    document.getElementById("inputboxprocselectype").style.display = "none"
+
+}
